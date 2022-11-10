66ababca3e8c2ebdb80c4126f3413da7c9a5c0b5
Gabriel Sasaki <gabrielpintosasaki@outlook.com>
2022-11-10 13:50:50 -0300

    Criação da classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace xadrez_console
+﻿using Tabuleiro;
+
+namespace xadrez_console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3, 4);
 
+            Console.WriteLine("Posição: " + p);
         }
     }
 }
